f4911cf7e3ae2a2d080e3e5f72660781e9b6e2a8
kingchangjun <nuj623@gmail.com>
2025-02-08 00:31:31 +0900

    1

---
+++ src/main/java/com/example/ttst/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.example.ttst.controller;
+
+
+import com.example.ttst.dto.ProductDto;
+import com.example.ttst.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/search")
+    public List<ProductDto> search(@RequestParam String keyword) {
+        return productService.getProducts(keyword);
+    }
+}
