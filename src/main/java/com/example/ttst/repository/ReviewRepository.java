9b18d579cdf8aa1a3377b6627dcbc693f05d66c5
kingchangjun <nuj623@gmail.com>
2025-02-14 17:53:38 +0900

    리뷰 기능 업그레이드
    ㅈㄱㄴ

--- src/main/java/com/example/ttst/repository/ReviewRepository.java
+++ src/main/java/com/example/ttst/repository/ReviewRepository.java
@@ -1,12 +1,15 @@
 package com.example.ttst.repository;
 
+import com.example.ttst.entity.Product;
 import com.example.ttst.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByProductId(Long productId);
+    Optional<Review> findByReviewCode(String reviewCode);
+    List<Review> findByProduct(Product product);
 }
